c2a88ae44cf9f4c1b4d88fd9f6ba1924f4308e88
Kaye Canoso <kaye_candy14@yahoo.com>
2017-05-08 12:06:00 +0800

    port

--- app.py
+++ app.py
@@ -32,5 +32,6 @@ def admin_login(  ):
 
 
 if __name__ == '__main__':
-	port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, port=port)
+	from os import environ
+	port = int( environ.get( 'PORT', 5000 ) )
+	app.run( debug=True, host='0.0.0.0', port=port )
